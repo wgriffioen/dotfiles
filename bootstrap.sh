5e3006a370ee930c4d9ee3e0da09844041939748
Wim Griffioen <wgriffioen@gmail.com>
2012-05-17 14:06:57 +0200

    created simple installation script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-echo 'Symlinking config files'
+echo 'Updating all dependencies'
+git submodule init
+git submodule update
+
+echo 'Symlinking all the files and directories'
+
+ln -s oh-my-zsh ~/.oh-my-zsh 
+ln -s vim ~/.vim
+ln -s vimrc ~/.vimrc
+ln -s gitconfig ~/.gitconfig
+ln -s gitignore ~/.gitignore
+ln -s zshrc ~/.zshrc
+
+echo 'Checking for rbenv installation'
+
+if [ -d "~/.rbenv"]
+    echo "rbenv is already installed"
+else
+    echo "rbenv is not installed. Let's get it:"
+    git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+fi
